0d23ff4676a2ffa6dd9634e7b54456da61a02dbf
LeeBoy <jobboy19890101@gmail.com>
2018-05-02 00:25:16 +0800

    feat: Basic Observables, Observers & Subscriptions

---
+++ src/helper.ts
@@ -0,0 +1,10 @@
+let helper = {
+  addItem (val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+  }
+}
+
+export default helper;
