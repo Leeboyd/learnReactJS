cbcf0e3c11ccf0e859fa6d05994876aad42b90cb
LeeBoy <jobboy19890101@gmail.com>
2018-05-13 13:44:36 +0800

    feat: From cold to hot observable

--- src/code.ts
+++ src/code.ts
@@ -1,21 +1,24 @@
 import { interval } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, publish } from 'rxjs/operators';
 import helper from './helper';
 
-let liveStreaming$ = interval(1000).pipe(
-  take(5)
+let publisher$ = interval(1000).pipe(
+  take(5),
+  publish()
 );
 
-liveStreaming$.subscribe(
+publisher$.subscribe(
   (val) => helper.addItem(`第一個 observer: ${val}`),
   error => console.error(error),
   () => helper.addItem(`第一個 observer complete`)
 );
 
 setTimeout(() => {
-  liveStreaming$.subscribe( 
+  publisher$.subscribe( 
     (val) => helper.addItem(`第二個 observer: ${val}`),
     error => console.error(error),
     () => helper.addItem(`第二個 observer complete`)
   ) 
-},2000)
+}, 3000);
+
+(publisher$ as any).connect();
