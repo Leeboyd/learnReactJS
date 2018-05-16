a427cd1b20762a6592185033a2d9ff40ac62fbda
LeeBoy <jobboy19890101@gmail.com>
2018-05-16 15:22:51 +0800

    feat: share

--- src/warm.ts
+++ src/warm.ts
@@ -1,27 +1,24 @@
-import { Observable } from 'rxjs';
-import { share } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { take, publish, refCount } from 'rxjs/operators';
 import helper from './helper';
 
-let stream$ = Observable.create((observer) => {
-  observer.next( 1 );
-  observer.next( 2 );
-  observer.next( 3 );
-  observer.complete();
-}).pipe(
-  share()
+let publisher$ = interval(1000).pipe(
+  take(5),
+  publish(),
+  refCount()
 );
 setTimeout(() => {
-  stream$.subscribe(
+  publisher$.subscribe(
     (val) => helper.addItem(`第一個 observer: ${val}`),
     error => console.error(error),
     () => helper.addItem(`第一個 observer complete`)
   );
-}, 1200)
+}, 1100);
 
 setTimeout(() => {
-  stream$.subscribe(
+  publisher$.subscribe( 
     (val) => helper.addItem(`第二個 observer: ${val}`),
     error => console.error(error),
     () => helper.addItem(`第二個 observer complete`)
-  );
-}, 2200)
+  ) 
+}, 3100);
